01637964e2537263cd7020e077b43eaf6dafdf05
lgc800430 <lgc800430@gmail.com>
2017-06-12 03:26:06 +0800

    50

--- CodingChallenges/CC_27_Fireworks_2D_p5/sketch.js
+++ CodingChallenges/CC_27_Fireworks_2D_p5/sketch.js
@@ -7,17 +7,17 @@ var fireworks = [];
 var gravity;
 
 function setup() {
-  createCanvas(400, 300);
+  createCanvas(800, 600);
   colorMode(HSB);
   gravity = createVector(0, 0.2);
   stroke(255);
   strokeWeight(4);
-  background(0);
+  background(51);
 }
 
 function draw() {
   colorMode(RGB);
-  background(0, 0, 0, 25);
+  background(50, 50, 50, 50);
   
   if (random(1) < 0.03) {
     fireworks.push(new Firework());
